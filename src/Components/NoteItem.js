5663a5f62e5bee05106645e84662101b14a30687
haninafis <haninafis@gmail.com>
2022-11-11 11:21:52 +0700

    Fix Typo

--- src/Components/NoteItem.js
+++ src/Components/NoteItem.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { showFormattedDate } from '../utils/index';
 
-function NoteItem ({tittle, createdAt, body}){
+function NoteItem ({title, createdAt, body}){
 
     return (
         <article className="note-item">
-            <h3 className="note-item__tittle">{tittle}</h3>
+            <h3 className="note-item__tittle">{title}</h3>
             <p className="note-item__createdAt">{showFormattedDate(createdAt)}</p>
             <p className="note-item__body">{body}</p>
         </article>
