b41bce686c346bbb5f4ea48d74ca06606f252fc7
haninafis <haninafis@gmail.com>
2022-11-13 09:21:55 +0700

    Adding Navigation

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import HomePage from './Pages/HomePage';
 import DetailPage from './Pages/DetailPage';
 import AddPage from './Pages/AddPage';
 import { Link } from 'react-router-dom';
+import Navigation from './Components/Navigation';
 
 function App() {
   return (
     <div className="app-container">
       <header>
         <h1><Link to="/">Aplikasi Catatan</Link></h1>
+        <Navigation/>
       </header>
       <main>
         <Routes>
---
+++ src/Components/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navigation() {
+    return (
+      <nav className="navigation">
+        <ul>
+            <li><a href="/archives">Arsip</a></li>
+        </ul>
+      </nav>
+    );
+}
+   
+export default Navigation;
