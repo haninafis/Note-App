f05ef8e071c15e61e0c751c3dad9b352dcfd4d36
Hani Nafis Muzaki <haninafis@gmail.com>
2022-12-07 15:10:30 +0700

    Add Authentication

--- src/App.js
+++ src/App.js
@@ -8,24 +8,50 @@ import { Link } from 'react-router-dom';
 import Navigation from './Components/Navigation';
 import NotFound from './Pages/NotFound';
 
-function App() {
-  return (
-    <div className="app-container">
-      <header>
-        <h1><Link to="/">Aplikasi Catatan</Link></h1>
-        <Navigation/>
-      </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<HomePage/>} />
-          <Route path="/notes/:id" element={<DetailPage/>} />
-          <Route path="/notes/new" element={<AddPage/>} />
-          <Route path="/archives" element={<ArchivedPage/>} />
-          <Route path="*" element={<NotFound/>} />
-        </Routes>
-      </main>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authedUser: null,
+    };
+  }
+  
+  render() {
+    if (this.state.authedUser === null) {
+      return (
+        <div className='app-container'>
+          <header>
+            <h1><Link to="/">Aplikasi Catatan</Link></h1>
+          </header>
+          <main>
+            <Routes>
+              <Route path="/*" element={<p>Halaman Login</p>} />
+              <Route path="/register" element={<p>Halaman Register</p>} />
+            </Routes>
+          </main>
+        </div>
+      )
+    }
+    
+    return (
+      <div className="app-container">
+        <header>
+          <h1><Link to="/">Aplikasi Catatan</Link></h1>
+          <Navigation/>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage/>} />
+            <Route path="/notes/:id" element={<DetailPage/>} />
+            <Route path="/notes/new" element={<AddPage/>} />
+            <Route path="/archives" element={<ArchivedPage/>} />
+            <Route path="*" element={<NotFound/>} />
+          </Routes>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
